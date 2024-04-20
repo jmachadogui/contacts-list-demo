a38589fbef635b0c29d57f8b64b53dd0fa1b0100
Guilherme Machado <guilherme.machado@mblabs.com.br>
2024-04-20 16:09:51 -0300

    feat: order contacts alphabetically

--- src/screens/Contacts/ContactsScreen.tsx
+++ src/screens/Contacts/ContactsScreen.tsx
@@ -5,10 +5,25 @@ import { useMemo } from "react"
 
 const ContactsScreen = () => {
 
+    const orderedContacts = useMemo(() => {
+        
+        return mockedContacts.sort((a, b) => {
+            const nameA = a.name.toUpperCase()
+            const nameB = b.name.toUpperCase()
+
+            if(nameA> nameB)
+                return 1
+            if(nameA < nameB)
+                return -1
+            return 0
+        })
+
+    }, [])
+
     return ( 
         <View style={{flex: 1, padding: 12}}>
             <Text>Lista de contatos</Text>
-            <ContactsList contacts={mockedContacts}/>
+            <ContactsList contacts={orderedContacts}/>
         </View>
     )
 }
